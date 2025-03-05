6f47706c8ac5058d0e18adf223c3f15f76a9d743
minhkhoiv12 <khoitk14033@gmail.com>
2025-03-05 11:26:56 +0700

    dwf

--- server.js
+++ server.js
@@ -27,12 +27,12 @@ app.use(
 app.use("/", contactRoute);
 
 // Phục vụ frontend khi ở production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client", "build")));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+//   );
+// }
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
